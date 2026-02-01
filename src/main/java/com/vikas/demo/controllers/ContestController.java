d7d9b9d5eaf1af94ec243cb7e20a0a81e114d910
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2026-02-02 00:34:45 +0530

    added put delete get and post for contest

--- src/main/java/com/vikas/demo/controllers/ContestController.java
+++ src/main/java/com/vikas/demo/controllers/ContestController.java
@@ -2,11 +2,14 @@ package com.vikas.demo.controllers;
 
 import com.vikas.demo.entity.ContestEntity;
 import com.vikas.demo.service.ContestServices;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contests")
@@ -23,5 +26,30 @@ public class ContestController {
     @GetMapping
     private ResponseEntity<?> getContest(){
         return contestServices.getAllContests();
+    }
+
+    @GetMapping("{contestId}")
+    private ResponseEntity<?> getContestInfo(@PathVariable ObjectId contestId){
+        try{
+            Optional<ContestEntity> contestInfo=contestServices.getContestInfo(contestId);
+            if(contestInfo.isPresent()){
+                return new ResponseEntity<>(contestInfo, HttpStatus.FOUND);
+            }else{
+                return new ResponseEntity<>("There is no Contest With ID :" +contestId,HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("{contestId}")
+    private ResponseEntity<?> deleteContest(@PathVariable ObjectId contestId){
+        return contestServices.deleteContestById(contestId);
+    }
+
+    @PutMapping("{contestId}")
+    private ResponseEntity<?> modifyContest(@PathVariable ObjectId contestId,@RequestBody ContestEntity contestEntity){
+        return contestServices.modifyContestById(contestId,contestEntity);
     }
 }
--- src/main/java/com/vikas/demo/service/ContestServices.java
+++ src/main/java/com/vikas/demo/service/ContestServices.java
@@ -2,13 +2,14 @@ package com.vikas.demo.service;
 
 import com.vikas.demo.entity.ContestEntity;
 import com.vikas.demo.repository.ContestRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ContestServices {
@@ -19,7 +20,6 @@ public class ContestServices {
     public ResponseEntity<?> addContest(ContestEntity contestEntity){
         try {
             contestRepository.save(contestEntity);
-
             return new ResponseEntity<>(contestEntity, HttpStatus.CREATED);
         }
         catch( Exception e){
@@ -30,11 +30,66 @@ public class ContestServices {
 
 
     public ResponseEntity<?> getAllContests(){
-
         List<ContestEntity> contestList=contestRepository.findAll();
         if(!contestList.isEmpty()){
             return new ResponseEntity<>(contestList, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public Optional<ContestEntity> getContestInfo(ObjectId contestId){
+        return contestRepository.findById(contestId);
+    }
+
+    public ResponseEntity<?> deleteContestById(ObjectId contestId){
+        try{
+
+            Optional<ContestEntity> contest=contestRepository.findById(contestId);
+            contestRepository.deleteById(contestId);
+            if(contest.isPresent()){
+                return new ResponseEntity<>("Contest with id : "+ contestId + " Deleted",HttpStatus.ACCEPTED);
+            }else{
+                return new ResponseEntity<>("Contest with id : "+ contestId +" Not found",HttpStatus.NOT_FOUND);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    public ResponseEntity<?> modifyContestById(ObjectId contestId,ContestEntity newContestInfo){
+        ContestEntity oldContestInfo = contestRepository.findById(contestId).orElse(null);
+        try{
+
+
+        if (oldContestInfo == null) {
+            return new ResponseEntity<>("Contest not found", HttpStatus.NOT_FOUND);
+        }
+
+        if (!newContestInfo.getContestName().isEmpty()) {
+            oldContestInfo.setContestName(newContestInfo.getContestName());
+        }
+
+        if (!newContestInfo.getContestDescription().isEmpty()) {
+            oldContestInfo.setContestDescription(newContestInfo.getContestDescription());
+        }
+
+        if (newContestInfo.getDuration() != 0) {
+            oldContestInfo.setDuration(newContestInfo.getDuration());
+        }
+
+        if (newContestInfo.getStartTime() != null) {
+            oldContestInfo.setStartTime(newContestInfo.getStartTime());
+        }
+
+        contestRepository.save(oldContestInfo);
+        return new ResponseEntity<>(oldContestInfo, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Contest Info Updated!",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
