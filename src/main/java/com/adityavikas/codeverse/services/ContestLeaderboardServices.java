d6e01e1ef77e3d49df99f4c24d7ebfec0eef8de0
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2026-06-25 12:13:53 +0530

    remove some headers to ensure there is no redundancy

--- src/main/java/com/adityavikas/codeverse/services/ContestLeaderboardServices.java
+++ src/main/java/com/adityavikas/codeverse/services/ContestLeaderboardServices.java
@@ -4,21 +4,17 @@ import com.adityavikas.codeverse.dto.ContestLeaderBoardResponseDTO;
 import com.adityavikas.codeverse.entity.ContestLeaderBoardEntity;
 import com.adityavikas.codeverse.repository.ContestLeaderBoardRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Service
 @AllArgsConstructor
 public class ContestLeaderboardServices {
